65210afc3c21749a26cb755c6c2babed7c0abc13
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-02-02 01:45:18 +0530

    .

--- Whatsapp Clone/js/clint.js
+++
@@ -1,8 +0,0 @@
-const socket = io('http://localhost:8000');
-
-const form = document.getElementById('msgcont');
-const msg = document.getElementById('msg');
-const container = document.querySelector(".container");
-
-const name = prompt("Enter Your Name To Join");
-socket.emit('new-user-join' , name );
