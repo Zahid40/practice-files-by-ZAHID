0bc69f6d70d4c69590b637a118ba820834c1c8e0
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-08-13 17:05:17 +0530

    .

--- c3website/resp.js
+++ c3website/resp.js
@@ -1,8 +1,18 @@
 burger = document.querySelector('.burger');
-navlist = document.querySelector('.navlist');
-rightnav = document.querySelector('.rightnav');
-
 burger.addEventListener('click', () => {
-    navlist.classList.toggle('dispalychanger');
-    rightnav.classList.toggle('dispalychanger');
+  document.querySelector('.navlist').classList.toggle('dispalychanger');    
   });
+//burger animation
+let showMenu = false;
+burger.addEventListener("click", toggleMenu);
+function toggleMenu() {
+    if (!showMenu) {
+        burger.classList.add("close");
+        // Reset the menu state
+        showMenu = true;
+    } else {
+        burger.classList.remove("close");
+        // Reset the menu state
+        showMenu = false;
+    }
+}
